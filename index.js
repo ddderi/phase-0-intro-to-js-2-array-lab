6d82ebf3c30d2286b4b930d5ebfd8736714d32f7
ddderi <maxime830@live.fr>
2022-01-02 14:14:29 +0100

    commit tree d937d38aa7cd7fb652f5a4514830ec775ecca356

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(){
+cats.push('Ralph');
+}
+
+function destructivelyPrependCat(){
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat()
+{
+    cats.shift()
+}
+
+
+function appendCat(){
+    const gato = [...cats, 'Broom'];
+    return gato;
+}
+
+function prependCat(){
+
+    const gato = ['Arnold', ...cats]
+    return gato;
+}
+
+function removeLastCat(){
+    const gato = cats.slice(0,2)
+    return gato;
+
+}
+
+function removeFirstCat(){
+    const gato = cats.slice(1)
+    return gato;
+}
